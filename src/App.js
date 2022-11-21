3758e732a9b8eaef372e40df98fe7631ac15df5e
dugle1605 <dugle1605@gmail.com>
2022-11-21 08:42:38 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Hero from "./components/Hero/Hero";
+import Slider from "./components/Slider/Slider";
+import Virtual from "./components/Virtual/Virtual";
+import Products from "./components/Products/Products";
+import Testimonials from "./components/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <span>Michael D-Killer</span>
+      <Header />
+      <Hero />
+      <Slider />
+      <Virtual />
+      <Products />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
