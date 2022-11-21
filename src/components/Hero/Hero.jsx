58405d525d5f247c6ff8ca95914d67497cb63134
Le Tien Dung <tien.dung.le@dksh.com>
2022-11-21 16:49:13 +0700

    commit code

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,8 +3,10 @@ import css from "./Hero.module.css";
 import HeroImg from "../../assets/hero.png";
 import { RiShoppingBagFill } from "react-icons/ri";
 import { BsArrowRight } from "react-icons/bs";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const transition = { duration: 3, type: "spring" };
   return (
     <div className={css.container}>
       {/* left side */}
@@ -23,9 +25,29 @@ const Hero = () => {
 
       {/* middle side hero image */}
       <div className={css.wrapper}>
-        <div className={css.blueCircle}></div>
-        <img src={HeroImg} alt="" width={600} />
-        <div className={css.cart2}>
+        {/* blue circle */}
+        <motion.div
+          initial={{ bottom: "2rem" }}
+          whileInView={{ bottom: "0rem" }}
+          className={css.blueCircle}
+          transition={transition}
+        ></motion.div>
+
+        {/* Hero image */}
+        <motion.img
+          transition={transition}
+          initial={{ bottom: "-2rem" }}
+          whileInView={{ bottom: "0rem" }}
+          src={HeroImg}
+          alt=""
+          width={600}
+        />
+        <motion.div
+          transition={transition}
+          initial={{ right: "4%" }}
+          whileInView={{ right: "2%" }}
+          className={css.cart2}
+        >
           <RiShoppingBagFill />
 
           <div className={css.signup}>
@@ -35,7 +57,7 @@ const Hero = () => {
               <BsArrowRight />
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
 
       {/* right side */}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./Header.module.css";
 import Logo from "../../assets/logo.png";
 import { CgShoppingBag } from "react-icons/cg";
+import { GoThreeBars } from "react-icons/go";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(true);
+  const toggleMenu = () => {
+    setShowMenu((showMenu) => !showMenu);
+  };
   return (
     <div className={css.container}>
       <div className={css.logo}>
@@ -11,14 +16,18 @@ const Header = () => {
         <span>amazon</span>
       </div>
       <div className={css.right}>
-        <div className={css.menu}>
-          <ul className={css.menu}>
-            <li>Collections</li>
-            <li>Brands</li>
-            <li>Sales</li>
-            <li>ENG</li>
-          </ul>
+        <div className={css.bars} onClick={toggleMenu}>
+          <GoThreeBars />
         </div>
+        <ul
+          className={css.menu}
+          style={{ display: showMenu ? "inherit" : "none" }}
+        >
+          <li>Collections</li>
+          <li>Brands</li>
+          <li>Sales</li>
+          <li>ENG</li>
+        </ul>
 
         <input type="text" className={css.search} placeholder="Search..." />
 
